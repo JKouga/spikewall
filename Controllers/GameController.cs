efb490aec1b523ddacee83d5266b1df1f5be02e2
JKouga <jkougarunners@gmail.com>
2025-08-04 22:50:58 -0400

    update exp system to make it similar to the original game

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -464,7 +464,9 @@ namespace spikewall.Controllers
                 }
 
                 // Character experience is based on how many rings were collected in the entire run
-                var exp = request.numRings + request.numFailureRings;
+                //var exp = request.numRings + request.numFailureRings;
+                // In the original game, character experience is based on 
+                var exp = (ulong)playerState.quickTotalHighScore / 5000;
 
                 sbyte charactersInRun = 1;
 
@@ -603,7 +605,14 @@ namespace spikewall.Controllers
                 }
 
                 // Character experience is based on how many rings were collected in the entire run
-                var exp = request.numRings + request.numFailureRings;
+                //var exp = request.numRings + request.numFailureRings;
+                // In the original game, character experience is based on totalHighScore / 5000;
+                // Character experience in boss showdowns is All-Or-Nothing (2000 for successful EXP, 0 for Failure)
+                var exp = (ulong)playerState.totalHighScore / 5000;
+                if (request.bossDestroyed != 0)
+                {
+                    exp = 2000;
+                }
 
                 sbyte charactersInRun = 1;
 
