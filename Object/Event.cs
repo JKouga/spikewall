25d1db89a4d7221341c05f92309d206eb919dc24
JKouga <jkougarunners@gmail.com>
2026-06-02 04:34:16 -0400

    create Event class

---
+++ Object/Event.cs
@@ -0,0 +1,11 @@
+﻿namespace spikewall.Object
+{
+    public class Event
+    {
+        public long? eventId { get; set; }
+        public long? eventType { get; set; }
+        public long? eventStartTime { get; set; }
+        public long? eventEndTime { get; set; }
+        public long? eventCloseTime { get; set; }
+    }
+}
