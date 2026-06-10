997948365cb408de7d5238587ad1f65d3976db6c
JKouga <jkougarunners@gmail.com>
2026-06-10 04:08:05 -0400

    update the EventListResponse object to take the Event class from Objects folder

--- Response/EventListResponse.cs
+++ Response/EventListResponse.cs
@@ -1,16 +1,16 @@
-﻿namespace spikewall.Response
+﻿using spikewall.Object;
+namespace spikewall.Response
 {
     /// <summary>
     /// Response containing event list (FIXME: Elaborate)
     /// </summary>
     public class EventListResponse : BaseResponse
     {
-        // FIXME: This is an array but shouldn't actually be strings, set up "Event" object
-        public string[]? eventList { get; set; }
+        public Event[]? eventList { get; set; }
 
         public EventListResponse()
         {
-            this.eventList = Array.Empty<string>();
+            this.eventList = Array.Empty<Event>();
         }
     }
 }
