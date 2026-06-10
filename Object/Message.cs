08a498ae2d8cc30706f420323b09e684c58c0c96
JKouga <jkougarunners@gmail.com>
2026-06-10 03:09:13 -0400

    add properties to the Message and StoreItem classes

--- Object/Message.cs
+++ Object/Message.cs
@@ -2,5 +2,12 @@
 {
     public class Message
     {
+        public string? Id { get; set; }
+        public long Type { get; set; }
+        public string? FriendId { get; set; }
+        public string? Name { get; set; }
+        public string? Url { get; set; }
+        public Item[]? MessageItem { get; set; }
+        public long ExpireTime { get; set; }
     }
 }
--- Object/StoreItem.cs
+++ Object/StoreItem.cs
@@ -2,5 +2,12 @@
 {
     public class StoreItem
     {
+        public string? StoreItemID { get; set; }
+        public string? ItemID { get; set; }
+        public string? PriceDisp { get; set; }
+        public string? ProductID  { get; set; }
+        public long NumItem { get; set; }
+        public long Price { get; set;  }
+        public Campaign[]? Campaign {  get; set; }
     }
 }
