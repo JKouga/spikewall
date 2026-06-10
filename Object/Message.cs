78cf08932d6e9723a98a0625e568f7175d18ad77
JKouga <jkougarunners@gmail.com>
2026-06-10 03:29:14 -0400

    update URL property name in the Message class

--- Object/Message.cs
+++ Object/Message.cs
@@ -6,7 +6,7 @@
         public long Type { get; set; }
         public string? FriendID { get; set; }
         public string? Name { get; set; }
-        public string? Url { get; set; }
+        public string? URL { get; set; }
         public Item[]? MessageItem { get; set; }
         public long ExpireTime { get; set; }
     }
