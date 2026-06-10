b352808d0339ee43ccdde9944ec6ffbabae2363d
JKouga <jkougarunners@gmail.com>
2026-06-10 04:11:19 -0400

    add Message object into the MessageListResponse class

--- Response/MessageListResponse.cs
+++ Response/MessageListResponse.cs
@@ -1,4 +1,6 @@
-﻿namespace spikewall.Response
+﻿using spikewall.Object;
+
+namespace spikewall.Response
 {
     /// <summary>
     /// Response containing list of messages
@@ -6,14 +8,14 @@
     public class MessageListResponse : BaseResponse
     {
         // FIXME: Messages shouldn't actually be strings, set up "Message" object
-        public string[]? messageList { get; set; }
+        public Message[]? messageList { get; set; }
         public long? totalMessage { get; set; }
         public string[]? operatorMessageList { get; set; }
         public long? totalOperatorMessage { get; set; }
 
         public MessageListResponse()
         {
-            messageList = Array.Empty<string>();
+            messageList = Array.Empty<Message>();
             totalMessage = 0;
             operatorMessageList = Array.Empty<string>();
             totalOperatorMessage = 0;
