68e4154288a0cae098a72d320b8c7367e2e112a5
JKouga <jkougarunners@gmail.com>
2026-06-10 03:25:47 -0400

    readd the Language enum class back into the repo

---
+++ Enums/Language.cs
@@ -0,0 +1,20 @@
+﻿namespace spikewall.Enums
+{
+    /// <summary>
+    /// This enum contains all the languages that are available in the game.
+    /// </summary>
+    public enum Language
+    {
+        Japanese,
+        English,
+        ChineseZHJ,
+        ChineseZH,
+        Korean,
+        French,
+        German,
+        Spanish,
+        Portuguese,
+        Italian,
+        Russian
+    }
+}
