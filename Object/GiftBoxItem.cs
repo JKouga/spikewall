a4e011b60aab6cd7aa795c2b402b8fbe3f434b2b
JKouga <jkougarunners@gmail.com>
2026-06-22 03:31:45 -0400

    add GiftBoxItem class to the object folder

---
+++ Object/GiftBoxItem.cs
@@ -0,0 +1,8 @@
+﻿namespace spikewall.Object
+{
+    public class GiftBoxItem : Item
+    {
+        public long? AdditionalInfo1 { get; set; }
+        public long? AdditionalInfo2 { get; set; }
+    }
+}
