3edad35b30a325bdef89f8954a6877c86d51d385
JKouga <jkougarunners@gmail.com>
2026-06-27 18:37:13 -0400

    add chao request object

---
+++ Request/ChaoRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace spikewall.Request
+{
+    public class ChaoRequest
+    {
+        [JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
+        public class EquipChaoRequest : BaseRequest
+        {
+            public int mainChaoId { get; set; }
+            public int subChaoId { get; set; }
+        }
+
+        [JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
+        public class CommitChaoWheelSpinRequest : BaseRequest
+        {
+            public int count { get; set; }
+        }
+    }
+}
