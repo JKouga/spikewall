14ec2f1e52ba0a3a8b2e86171537fa4f203c34cd
JKouga <jkougarunners@gmail.com>
2026-06-30 14:33:51 -0400

    add 1 to the max to include the end of the chaoweights

--- Controllers/ChaoController.cs
+++ Controllers/ChaoController.cs
@@ -175,7 +175,7 @@ namespace spikewall.Controllers
                     switch (wonItemID)
                     {
                         case (ulong)Item.ItemID.RareEgg:
-                            var getRareChaoPrizeSql = Db.GetCommand(@"SELECT * FROM `sw_chaorouletteprizelist` WHERE rarity = '{0}' AND chao_weight >= RAND() * (SELECT MAX(chao_weight) FROM `sw_chaorouletteprizelist`) ORDER BY chao_weight LIMIT 1", (ulong)Item.ItemID.RareEgg);
+                            var getRareChaoPrizeSql = Db.GetCommand(@"SELECT * FROM `sw_chaorouletteprizelist` WHERE rarity = '{0}' AND chao_weight >= RAND() * (SELECT MAX(chao_weight)+1 FROM `sw_chaorouletteprizelist`) ORDER BY chao_weight LIMIT 1", (ulong)Item.ItemID.RareEgg);
                             var getRareChaoPrizeCommand = new MySqlCommand(getRareChaoPrizeSql, conn);
                             var rareChaoPrizeRdr = command.ExecuteReader();
 
@@ -198,7 +198,7 @@ namespace spikewall.Controllers
                             
                             break;
                         case (ulong)Item.ItemID.SuperRareEgg:
-                            var getSRareChaoPrizeSql = Db.GetCommand(@"SELECT * FROM `sw_chaorouletteprizelist` WHERE rarity = '{0}' AND chao_weight >= RAND() * (SELECT MAX(chao_weight) FROM `sw_chaorouletteprizelist`) ORDER BY chao_weight LIMIT 1", (ulong)Item.ItemID.SuperRareEgg);
+                            var getSRareChaoPrizeSql = Db.GetCommand(@"SELECT * FROM `sw_chaorouletteprizelist` WHERE rarity = '{0}' AND chao_weight >= RAND() * (SELECT MAX(chao_weight)+1 FROM `sw_chaorouletteprizelist`) ORDER BY chao_weight LIMIT 1", (ulong)Item.ItemID.SuperRareEgg);
                             var getSRareChaoPrizeCommand = new MySqlCommand(getSRareChaoPrizeSql, conn);
                             var sRareChaoPrizeRdr = getSRareChaoPrizeCommand.ExecuteReader();
                             if (sRareChaoPrizeRdr.HasRows)
@@ -219,7 +219,7 @@ namespace spikewall.Controllers
                             }
                             break;
                         case (ulong)Item.ItemID.CharacterEgg:
-                            var characterPrizeSql = Db.GetCommand(@"SELECT * FROM `sw_chaorouletteprizelist` WHERE rarity = '{0}' AND chao_weight >= RAND() * (SELECT MAX(chao_weight) FROM `sw_chaorouletteprizelist`) ORDER BY chao_weight LIMIT 1", (ulong)Item.ItemID.CharacterEgg);
+                            var characterPrizeSql = Db.GetCommand(@"SELECT * FROM `sw_chaorouletteprizelist` WHERE rarity = '{0}' AND chao_weight >= RAND() * (SELECT MAX(chao_weight)+1 FROM `sw_chaorouletteprizelist`) ORDER BY chao_weight LIMIT 1", (ulong)Item.ItemID.CharacterEgg);
                             var characterPrizeCommand = new MySqlCommand(characterPrizeSql, conn);
                             var characterPrizeRdr = characterPrizeCommand.ExecuteReader();
                             if (characterPrizeRdr.HasRows)
