8ddb28014f1482a132e407d291867a6c8a2ff10a
JKouga <jkougarunners@gmail.com>
2026-07-06 01:36:34 -0400

    update the Event project to place the enums within the Event Object and add EventState class

--- Object/Event.cs
+++ Object/Event.cs
@@ -11,53 +11,59 @@
         public long? eventStartTime { get; set; }
         public long? eventEndTime { get; set; }
         public long? eventCloseTime { get; set; }
+
+        /// <summary>
+        /// Enum that contains all the Event IDs
+        /// </summary>
+        public enum EventID
+        {
+            SpecialStage = 100000000,
+            RaidBoss,
+            CollectObject,
+            Gacha,
+            Advert,
+            Quick,
+            BGM
+        }
+
+        /// <summary>
+        /// Enum that contains all the Event Types in the game
+        /// </summary>
+        public enum EventType
+        {
+            SpecialStage,
+            RaidBoss,
+            CollectObject,
+            Gacha,
+            Advert,
+            Quick,
+            BGM
+        }
+
+        /// <summary>
+        /// Enum that contains the Advert Event Types
+        /// </summary>
+        public enum AdvertEventType
+        {
+            Roulette,
+            Character,
+            Shop
+        }
+
+        /// <summary>
+        /// Evnum that contains the Collect Event Types
+        /// </summary>
+        public enum CollectEventType
+        {
+            GetAnimals,
+            GetRings,
+            RunDistance
+        }
     }
 
-    /// <summary>
-    /// Enum that contains all the Event IDs
-    /// </summary>
-    public enum EventID
+    public class EventState
     {
-        SpecialStage = 100000000,
-        RaidBoss,
-        CollectObject,
-        Gacha,
-        Advert,
-        Quick,
-        BGM
-    }
-
-    /// <summary>
-    /// Enum that contains all the Event Types in the game
-    /// </summary>
-    public enum EventType
-    {
-        SpecialStage,
-        RaidBoss,
-        CollectObject,
-        Gacha,
-        Advert,
-        Quick,
-        BGM
-    }
-
-    /// <summary>
-    /// Enum that contains the Advert Event Types
-    /// </summary>
-    public enum AdvertEventType
-    {
-        Roulette,
-        Character,
-        Shop
-    }
-
-    /// <summary>
-    /// Evnum that contains the Collect Event Types
-    /// </summary>
-    public enum CollectEventType
-    {
-        GetAnimals,
-        GetRings,
-        RunDistance
+        public long Param { get; set; }
+        public long RewardID { get; set; }
     }
 }
