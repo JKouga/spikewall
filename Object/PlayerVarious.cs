f7e4fdf75afd5dade1d722245a904b2c5266a10b
JKouga <jkougarunners@gmail.com>
2026-08-16 04:16:24 -0400

    add the PlayerVerious.cs file and add the class into the Object Directory

---
+++ Object/PlayerVarious.cs
@@ -0,0 +1,13 @@
+﻿namespace spikewall.Object
+{
+    public class PlayerVarious
+    {
+        public long? CmSkipCount { get; set; }
+        public long? EnergyRecoveryMax { get; set; }
+        public long? EnergyRecoveryTime { get; set; }
+        public long? OnePlayCmCount { get; set; }
+        public long? OnePlayContinueCount { get; set; }
+        public long? IsPurchased { get; set; }
+        public long? HideLeague { get; set; }
+    }
+}
