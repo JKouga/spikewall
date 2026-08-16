2158b250eb25f0141f61df468cada25128d981e7
JKouga <jkougarunners@gmail.com>
2026-08-16 03:48:15 -0400

    add the RouletteInfo class into the Object directory

---
+++ Object/RouletteInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace spikewall.Object
+{
+    public class RouletteInfo
+    {
+        public long? LoginRouletteID { get; set; }
+        public long? RoulettePeriodEnd { get; set; }
+        public long? RouletteCountInPeriod { get; set; }
+        public bool? GotJackpotThisPeriod { get; set; }
+    }
+}
