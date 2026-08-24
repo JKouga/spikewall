578c1502da2267a18ad52c90193f7b7912158d76
JKouga <jkougarunners@gmail.com>
2026-08-24 00:09:58 -0400

    move the firstloginbonus query into the loginbonus sql file

--- Db/sqlfiles/loginbonus.sql
+++ Db/sqlfiles/loginbonus.sql
@@ -1,4 +1,34 @@
+DROP TABLE IF EXISTS `sw_firstloginbonus`;
+
 DROP TABLE IF EXISTS `sw_loginbonus`;
+
+CREATE TABLE
+  `sw_firstloginbonus` (
+    day TINYINT NOT NULL,
+    item1 MEDIUMINT NOT NULL DEFAULT 900000,
+    item1_count BIGINT UNSIGNED NOT NULL DEFAULT 5,
+    item2 MEDIUMINT NOT NULL DEFAULT 900000,
+    item2_count BIGINT UNSIGNED NOT NULL DEFAULT 5,
+    item3 MEDIUMINT NOT NULL DEFAULT 900000,
+    item3_count BIGINT UNSIGNED NOT NULL DEFAULT 5,
+  );
+
+INSERT INTO
+  `sw_firstloginbonus` (
+    day,
+    item1,
+    item1_count,
+    item2,
+    item2_count,
+    item3,
+    item3_count
+  )
+VALUES
+  ('1','900000','10','910000','10000','240000','3'),
+  ('2','900000','20','910000','20000','240000','3'),
+  ('3','900000','20','910000','20000','240000','3'),
+  ('4','230000','1','910000','50000','240000','3'),
+  ('5','300013','1','910000','50000','240000','3');
 
 CREATE TABLE
   `sw_loginbonus` (
@@ -52,3 +82,4 @@ VALUES
   ('4', '5', '900000', '15', '910000', '15000', '240000', '4'),
   ('4', '6', '900000', '15', '910000', '15000', '240000', '4'),
   ('4', '7', '900000', '20', '910000', '20000', '24000', '5');
+
